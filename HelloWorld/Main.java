685bc7b674322c8f5cd31dedf80ba9e51a94fcf5
david1402 <49765318+david1402@users.noreply.github.com>
2019-05-02 20:28:24 +0200

    Update Main.java

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         boolean flag = true;
         System.out.println("Hello world");
+        System.out.println("Hello David");
         System.out.println("Hello from Andrew");
         System.out.println("Hello again");
+        System.out.println("and again");
         while(flag != false) {
             System.out.println("I'm alive!");
             Thread.sleep(1000);
